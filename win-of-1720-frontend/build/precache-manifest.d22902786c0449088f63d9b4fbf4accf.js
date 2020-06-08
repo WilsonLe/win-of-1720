91750d667c0765954286108bda8ad66830fb643c
WilsonLe <leanhminh2907@gmail.com>
2020-06-08 19:45:40 +0700

    first commit

---
+++ win-of-1720-frontend/build/precache-manifest.d22902786c0449088f63d9b4fbf4accf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b51444f0dc4b122d506f23edba49c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50f130a22c4887576fce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "55b0c9e4c42776aecbb7",
+    "url": "/static/js/2.a3b573bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3b573bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f130a22c4887576fce",
+    "url": "/static/js/main.f8309f0b.chunk.js"
+  },
+  {
+    "revision": "955ff611b5e80d5c06d5",
+    "url": "/static/js/runtime-main.940a75d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
