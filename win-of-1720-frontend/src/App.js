86d67ec0bd69c64f53c82d767a6bd33c19c930dd
WilsonLe <leanhminh2907@gmail.com>
2020-06-08 19:56:59 +0700

    dat config file :(((

--- win-of-1720-frontend/src/App.js
+++ win-of-1720-frontend/src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import WinXP from 'WinXP';
+import { useGA } from 'hooks';
+
+const App = () => {
+  useGA('UA-135848027-3', 'winXP');
+  return <WinXP />;
+};
 
 export default App;
