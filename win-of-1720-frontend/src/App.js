a53b4e249c1c912fb50809b02b54f726f5dda346
WilsonLe <leanhminh2907@gmail.com>
2020-06-08 19:42:41 +0700

    reverttttttttttttttttttttttttttttttt

--- win-of-1720-frontend/src/App.js
+++ win-of-1720-frontend/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Win_of_1720 from 'Win_of_1720';
-
-const App = () => {
-  return <Win_of_1720 />;
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
